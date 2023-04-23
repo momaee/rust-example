ad134b6b74dfeb78246b2aa6a143f97425caa5a0
Mohammad Mahmoodian <drmo.ma@yahoo.com>
2023-04-23 13:24:19 +0200

    feat: dice bot using teloxide
    Signed-off-by: Mohammad Mahmoodian <drmo.ma@yahoo.com>

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,15 @@
-use std::rc::Rc;
-use std::cell::RefCell;
+use teloxide::prelude::*;
 
-fn main() {
-    let s1 = Rc::new(RefCell::new(String::from("hello")));
-    let s2 = s1;
+#[tokio::main]
+async fn main() {
+    pretty_env_logger::init();
+    log::info!("Starting throw dice bot...");
 
-    // This line will not compile because s1 has been moved to s2
-    println!("s1 = {}", s1.borrow());
+    let bot = Bot::from_env();
 
-    // This line will compile because s2 is still in scope
-    println!("s2 = {}", s2.borrow());
-
-    let mut s3 = String::from("hello");
-    let len = calculate_length(&s3);
-    println!("The length of '{}' is {}.", s3, len);
-
-    change(&mut s3);
-    println!("s3 = {}", s3);
-}
-
-fn calculate_length(s: &String) -> usize {
-    s.len()
-}
-
-fn change(s: &mut String) {
-    s.push_str(", world");
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await;
 }
